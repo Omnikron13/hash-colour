5ea285b71418372e26bdcaaeebca836f7aedb6bf
Joey Sabey <joey.sabey@gmx.com>
2024-05-28 06:20:42 +0100

    feature: add functions to hash a string to a HSLuv colour, taking either 32 or 24 bits for the hue range

---
+++ main.go
@@ -0,0 +1,63 @@
+package hash_colour
+
+import (
+   "fmt"
+   _ "fmt"
+   "image/color"
+   "math/bits"
+
+   "github.com/cespare/xxhash"
+   gc "github.com/lucasb-eyer/go-colorful"
+)
+
+const MASK5BIT = 0b11111
+const MASK6BIT = 0b111111
+const MASK7BIT = 0b1111111
+const MASK8BIT = 0xFF
+const MASK24BIT = 0xFFFFFF
+const MASK32BIT = 0xFFFFFFFF
+
+const SAT_FLOOR = 28
+const LUV_FLOOR = 28
+
+func Colour5532(s string) (sat uint64, luv uint64, hue float64) {
+   hash := xxhash.Sum64String(s)
+   sat = take_bits(&hash, MASK5BIT)
+   luv = take_bits(&hash, MASK5BIT)
+   hue = float64(take_bits(&hash, MASK32BIT) & MASK32BIT) / float64(MASK32BIT) * 360
+   return
+}
+
+func Colour5524(s string) (sat uint64, luv uint64, hue float64) {
+   hash := xxhash.Sum64String(s)
+   sat = take_bits(&hash, MASK5BIT)
+   luv = take_bits(&hash, MASK5BIT)
+   hue = float64(take_bits(&hash, MASK24BIT) & MASK24BIT) / float64(MASK24BIT) * 360
+   return
+}
+
+func HSLuv(s string) (c color.Color) {
+   sat, luv, hue := Colour5532(s)
+   c = gc.HSLuv(hue, float64(sat+SAT_FLOOR)/100, float64(luv+LUV_FLOOR)/100)
+   return
+}
+
+func HPLuv24(s string) (c color.Color) {
+   sat, luv, hue := Colour5524(s)
+   c = gc.HPLuv(hue, float64(sat+LUV_FLOOR)/100, float64(luv+LUV_FLOOR)/100)
+   return
+}
+
+func HexColour(c color.Color) (hex string) {
+   r, g, b, _ := c.RGBA()
+   hex = "#" + fmt.Sprintf("%0x", r) + fmt.Sprintf("%0x", g) + fmt.Sprintf("%0x", b)
+   return
+}
+
+func take_bits(hash *uint64, mask uint64) (result uint64) {
+   result = *hash & mask
+   h := bits.RotateLeft64(*hash, -bits.OnesCount64(mask))
+   hash = &h
+   return
+}
+
